0e2c86669e8043274f5262741713bd93b0b95423
Camilo-Charry <katcamcharryperez@gmail.com>
2025-08-13 18:28:18 -0500

    interfaz de inicio

--- src/app/auth/auth.routers.ts
+++ src/app/auth/auth.routers.ts
@@ -1,34 +1,40 @@
 import { Routes } from "@angular/router";
 
-import { L } from "@angular/cdk/keycodes";
 import { LoginComponent } from "./entry/pages/loginGeneral/login/login.component";
-import { InicioComponent } from "./entry/pages/inicio/inicio.component";
 import { ContactComponent } from "./entry/pages/contact/contact.component";
 import { ForgottenPasswordComponent } from "./entry/pages/loginGeneral/forgotten-password/forgotten-password.component";
 import { VerificationCodeComponent } from "./entry/pages/loginGeneral/verification-code/verification-code.component";
 import { NewPasswordComponent } from "./entry/pages/loginGeneral/new-password/new-password.component";
 
+// Componente de bienvenida
+import { IngresarComponent } from "./entry/pages/ingresar/ingresar.component";
 
-
+// Bienvenida (pantalla principal)
 export const inicioRoutes: Routes = [
-  { path: '', component: InicioComponent }
+  { path: '', component: IngresarComponent }
 ];
 
+// Contacto
 export const contactRoutes: Routes = [
   { path: '', component: ContactComponent }
 ];
 
+// Login
 export const loginRoutes: Routes = [
-    {path: '',component: LoginComponent}
+  { path: '', component: LoginComponent }
 ];
 
+// Recuperar contraseña
 export const ForgottenPasswordRoutes: Routes = [
-    {path: '', component: ForgottenPasswordComponent}
+  { path: '', component: ForgottenPasswordComponent }
 ];
 
+// Verificación de código
 export const verificationCodeRoutes: Routes = [
-  {path: '',component: VerificationCodeComponent}
+  { path: '', component: VerificationCodeComponent }
 ];
+
+// Nueva contraseña
 export const newPasswordCodeRoutes: Routes = [
-  {path: '', component: NewPasswordComponent}
+  { path: '', component: NewPasswordComponent }
 ];
