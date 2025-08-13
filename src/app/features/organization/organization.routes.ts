ed17d36d430da3a13f42e129ec3233cb73af5efe
Camilo-Charry <katcamcharryperez@gmail.com>
2025-08-13 00:11:49 -0500

    sucursal mapa,falta agregarle modal

--- src/app/features/organization/organization.routes.ts
+++ src/app/features/organization/organization.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 
 import { EstructuraOrganizativaComponent } from './pages/estructura-organizativa/estructura-organizativa.component';
-import { SurcursalComponent } from './location/sucursales/pages/surcursal/surcursal.component';
+import { SucursalComponent } from './location/sucursales/pages/surcursal/surcursal.component';
 import { UnidadOrganizativaComponent } from './structure/unidades-organizativa/pages/unidad-organizativa/unidad-organizativa.component';
 import { DivisionesInternasComponent } from './structure/divisines-Internal/pages/divisiones-internas/divisiones-internas.component';
 import { PerfilesComponent } from './assignment/perfile/pages/perfiles/perfiles.component';
@@ -13,9 +13,10 @@ export const organizationRoutes: Routes = [
     component: EstructuraOrganizativaComponent
   },
   {
-    path: 'sucursales',
-    component: SurcursalComponent
-  },
+  path: 'sucursales',
+  component: SucursalComponent
+}
+,
   {
     path: 'unidades-organizativas',
     component: UnidadOrganizativaComponent
