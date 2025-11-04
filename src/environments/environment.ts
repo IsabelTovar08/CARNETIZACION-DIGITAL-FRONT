91d4ccda70ae62d976b2641faed80973e4290e5e
Isabel Tovar <Isabeltovarp.18@gmail.com>
2025-11-04 07:43:21 -0500

    Staging

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,5 +5,5 @@
 
 export const environment = {
   production: true,
-  URL: (window as any)['env']?.API_BASE_URL || 'API_BASE_URL=http://localhost:5100',
+  URL: (window as any)['env']?.API_BASE_URL,
 };
